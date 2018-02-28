9b6cb4a4735e5e0d9f49cacb056eb1e20961022d
Steven Starwalt <steven.starwalt@yahoo.com>
2018-02-28 09:42:57 -0800

    added the error.js

---
+++ scripts/views/error-view.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = app || {};
+
+(function (module) {
+  const errorView = {};
+
+  errorView.initErrorPage = function(err) {
+    $('.container').hide();
+    $('.errorView').show();
+    $('#errorMessage').empty();
+    let template = Handlebars.compile($('#errTemplate').text());
+    $('#errorMessage').append(template(err));
+  };
+
+  module.errorView = errorView;
+})(app)
